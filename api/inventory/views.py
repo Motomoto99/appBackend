52817643b2ba208ac503a0abf7ac3d8dfc41347b
Motomoto99 <motoshou39@icloud.com>
2024-06-04 14:09:58 +0000

    inventory シリアライザーとURLマッピングの追加

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+
+class ProductView(APIView):
+    """
+    商品操作に関数関数
+    """
+    def get(self, request, format=None):
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
