162863e87ec042859f7e5d632a64c988e475100f
Motomoto99 <motoshou39@icloud.com>
2024-06-10 14:23:05 +0000

    リクエストに対する処理の記述

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 from rest_framework.views import APIView
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+# from rest_framework.viewsets import ModelViewSet
 from .models import Product
-from .serializers import ProductSerializer
+from .serializers import ProductSerializer,Purchaseserializer,SaleSerializer
 from rest_framework import status
 
 class ProductView(APIView):
@@ -16,4 +18,56 @@ class ProductView(APIView):
         queryset = Product.objects.all()
         serializer = ProductSerializer(queryset, many=True)
         return Response(serializer.data, status.HTTP_200_OK)
+    
+    # 商品操作に関する関数で共通で使用する商品取得関数
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            raise NotFound
 
+    def get(self, request, id=None, format=None):
+        # 商品の一覧もしくは一意の商品を取得する
+        if id is None :
+            queryset = Product.objects.all()
+            serializer = ProductSerializer(queryset, many=True)
+        else:
+            product = self.get_object(id)
+            serializer = ProductSerializer(product)
+        return Response(serializer.data, status.HTTP_200_OK)
+    
+    def post(self, request, format=None):
+        serializer = ProductSerializer(data=request.data)
+        # validationを通らなかった場合、例外を投げる
+        serializer.is_valid(raise_exception=True)
+        # 検証したデータを永続化する
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    
+    def put(self, request, id, format=None):
+        product = self.get_object(id)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class PurchaseView(APIView):
+    def post(self, request, format=None):
+        """
+        仕入情報を登録する
+        """
+        serializer = Purchaseserializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    
+class SalesView(APIView):
+    def post(self, request, format=None):
+        """
+        売上情報を登録する
+        """
+        serializer = SaleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status.HTTP_201_CREATED)
+    
